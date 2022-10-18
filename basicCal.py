77ab3d20c11985382aac61a53001fb252e53af03
saumya1112 <saumya.gupta_cs18@gla.ac.in>
2022-10-18 16:44:46 +0530

    mul

--- basicCal.py
+++ basicCal.py
@@ -5,18 +5,14 @@ def add(a,b):
     print(a+b)
 
 add(3,4)
-<<<<<<< Updated upstream
 
-<<<<<<< HEAD
 def mul(a,b):
     print(a*b)
 
 mul(2,3)
-=======
+
 def sub(a,b):
     print(a-b)
 
 sub(1,2)
->>>>>>> bcbf2c96fb5de39b0d8cb6fb1b9110e358044322
-=======
->>>>>>> Stashed changes
+
